c9a988ea6ff686721dbfcb8b5a521ca9c9e85b09
IWBTM <rnlduadl2000@naver.com>
2022-12-26 19:37:34 +0900

    회원가입 구현 완료

--- click-go-project/src/main/java/com/clickgo/project/ClickGoProjectApplication.java
+++ click-go-project/src/main/java/com/clickgo/project/ClickGoProjectApplication.java
@@ -9,5 +9,4 @@ public class ClickGoProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ClickGoProjectApplication.class, args);
 	}
-
 }
--- click-go-project/src/main/java/com/clickgo/project/controller/UserController.java
+++ click-go-project/src/main/java/com/clickgo/project/controller/UserController.java
@@ -6,4 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
+	@GetMapping("/auth/login_form")
+	public String login_form() {
+		System.out.println("z");
+		return "/user/login";
+	}
+
+	@GetMapping("/auth/join_form")
+	public String join_form() {
+		System.err.println("zz");
+		return "/user/join_form";
+	}
 }
