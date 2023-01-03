997547516632552933f5fc28ea50776bf8d7a204
ji_euni <ji_euni@naver.com>
2023-01-03 11:41:10 +0900

    가맹점 목록 완료

--- click-go-project/src/main/java/com/clickgo/project/service/UserService.java
+++ click-go-project/src/main/java/com/clickgo/project/service/UserService.java
@@ -126,39 +126,5 @@ public class UserService {
 		});
 	}
 	
-	 //랜덤함수로 임시비밀번호 구문 만들기
 	
-	// 임시비밀번호로 DB저장
-	@Transactional
-	public String searchPasswordChange(String email) {
-		User userEntity = userRepository.findByEmail(email).orElseThrow(() -> {
-			return new IllegalArgumentException("찾을 수 없는 회원입니다.");
-		});
-		String rawPassword = "";
-		if (userEntity.getLoginType() == null || userEntity.getLoginType().equals("")) {
-			
-			rawPassword = getTempPassword();
-			String encPassword = encoder.encode(rawPassword);
-
-			userEntity.setPassword(encPassword);
-			
-		}
-		return rawPassword;
-	}
-	
-	// 임시비밀번호 생성
-    public String getTempPassword(){
-        char[] charSet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
-                'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
-
-        String str = "";
-
-        // 문자 배열 길이의 값을 랜덤으로 10개를 뽑아 구문을 작성함
-        int idx = 0;
-        for (int i = 0; i < 10; i++) {
-            idx = (int) (charSet.length * Math.random());
-            str += charSet[idx];
-        }
-        return str;
-    }
 }
